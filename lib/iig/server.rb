d22128bcbef60c15996bdab1ae1162124780f805
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-19 19:32:27 +0900

    add method argument

--- lib/iig/server.rb
+++ lib/iig/server.rb
@@ -3,14 +3,14 @@ require 'slop'
 
 module InterestIrcGateway
   class Server < Net::IRC::Server
-    def initialize
-      opts = parse_options
-      super(opts[:server], opts[:port], Session, opts)
+    def initialize(opts = nil)
+      opts ||= parse_options
+      super(opts[:server], opts[:port], InterestIrcGateway::Session, opts)
     end
 
     def parse_options
       opts = Slop.parse(help: true) do
-        banner "Usage: iig [options]"
+        banner 'Usage: iig [options]'
         on :p, :port,    'Port number to listen (default: 16704)',                 argument: :optional, as: :integer, default: 16704
         on :s, :server,  'Host name or IP address to listen (default: localhost)', argument: :optional, as: :string,  default: :localhost
         on :w, :wait,    'Wait SECONDS between retrievals (default: 3600)',        argument: :optional, as: :integer, default: 3600
