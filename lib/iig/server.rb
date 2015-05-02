63fd727052f505a230f75dc8299afdb92afcffe6
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-05-02 22:35:39 +0900

    Fix description

--- lib/iig/server.rb
+++ lib/iig/server.rb
@@ -11,7 +11,7 @@ module InterestIrcGateway
     def parse_options
       opts = Slop.parse do |o|
         o.integer '-p', '--port', 'Port number to listen (default: 16704)', default: 16704
-        o.string '-h', '--host',  'Host name or IP address to listen (default: localhost)', default: '0.0.0.0'
+        o.string '-h', '--host',  'Host name or IP address to listen (default: 0.0.0.0)', default: '0.0.0.0'
         o.integer '-w', '--wait', 'Wait SECONDS between retrievals (default: 3600)', default: 3600
         o.string '-l', '--log',   'Log file (default: STDOUT)', default: nil
         o.on '-v', '--version', 'Print the version' do
